6b7159b00873e3b28c6660b41e489cdf7b9f1181
Mubarak Ganiyu <mubarak.a.ganiyu@vanderbilt.edu>
2022-11-03 16:43:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -36,9 +36,9 @@ def face_detector(img):
     #elif img.shape == 2:
     #    gray = img
         
-    if (img.shape[0] == 48) & (img.shape[1] == 48) & (img.shape == 2):
+    if (img.shape == 2):
         new_img = PIL.Image.fromarray(img)
-        #new_img = new_img.resize((48,48)).convert("L")
+        new_img = new_img.resize((48,48)).convert("L")
         return new_img
       
     elif img.shape != 2:
