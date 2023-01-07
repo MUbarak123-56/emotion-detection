62a650dc56ee306c94f239a958157dc6906665e5
Mubarak Ganiyu <mubarak.a.ganiyu@vanderbilt.edu>
2023-01-07 17:54:19 -0600

    Update app.py

--- app.py
+++ app.py
@@ -98,14 +98,14 @@ def run():
         with open(image_file, "rb") as image_file:
             encoded_string = base64.b64encode(image_file.read())
         st.markdown(
-        '''
+        f"""
         <style>
-        .stApp {{ 
-            background-image: url(data:image/png;base64,%s);
+        .stApp {{
+            background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
             background-size: cover
          }}
          </style>
-         '''%encoded_string.decode(),
+         """,
          unsafe_allow_html=True
          )
     
